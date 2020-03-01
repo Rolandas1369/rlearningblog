4b7130a523fc056c4588e610e61db1c1387c1f3c
Rolandas <rolandaswbu@gmail.com>
2020-03-01 14:02:50 +0200

    / path

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import Blog from './components/blog';
+import Post from './components/post';
 
 const BaseRouter = () => (
     <div>
+          <Route exact path="/" component={Post}/>{" "}
           <Route exact path="/more" component={Blog}/>{" "}
     </div>
   );
