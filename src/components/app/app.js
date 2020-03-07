41bc307d9c6250a5ff8702041d49970e46160e0c
Rolandas <rolandaswbu@gmail.com>
2020-03-08 00:20:34 +0200

    permision classes

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -82,6 +82,24 @@ export default class App extends Component {
     }
 
     addItem = (item, content, image) => {
+
+        function getCookie(name) {
+            var cookieValue = null;
+            if (document.cookie && document.cookie != '') {
+                var cookies = document.cookie.split(';');
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = cookies[i].trim();
+                    // Does this cookie string begin with the name we want?
+                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
+
+        let cokie = getCookie('csrftoken');
         
         console.log("item=>", item, "conte=>", content, image)
         
