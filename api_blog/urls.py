dd51ffab1404756c33eac2d9884ad887d497cae5
Rolandas <rolandaswbu@gmail.com>
2020-03-07 22:41:53 +0200

    permision classes

--- api_blog/urls.py
+++ api_blog/urls.py
@@ -7,13 +7,14 @@ from .views import (
     PostListView,
     PostDetail,
     DeletePost,
-    CreateAPIView 
+    CreateAPIView,
+    PostCreateView
 )
 
 urlpatterns = [
     path('posts/', PostListView.as_view()),
     path('posts/<int:pk>/', PostDetail.as_view()),
     path('posts/delete/<int:pk>/', DeletePost.as_view()),
-    path('posts/create/', CreateAPIView.as_view())
+    path('posts/create/', PostCreateView.as_view())
     
 ]
