96f9f0ee3e98e01f2a050ddaf376703ce2c08ad7
Rolandas <rolandaswbu@gmail.com>
2020-03-09 00:17:39 +0200

    images

--- api_core/urls.py
+++ api_core/urls.py
@@ -26,6 +26,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api_blog.urls')),
+    path('ok', TemplateView.as_view(template_name='index.html'))
     # re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
 
