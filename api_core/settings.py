9fb377ad8eb9b06e747c69f2f154da9c310d9a9c
Rolandas <rolandaswbu@gmail.com>
2020-03-08 23:55:26 +0200

    images

--- api_core/settings.py
+++ api_core/settings.py
@@ -149,14 +149,7 @@ CORS_ORIGIN_WHITELIST = (
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-# CORS_ALLOW_METHODS = [
-#     'DELETE',
-#     'GET',
-#     'OPTIONS',
-#     'PATCH',
-#     'POST',
-#     'PUT',
-# ]
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "build/static/media")
 
