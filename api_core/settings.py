47cf401e2252373d468861a2ef7f2d4521a51989
Rolandas <rolandaswbu@gmail.com>
2020-03-08 23:49:00 +0200

    images

--- api_core/settings.py
+++ api_core/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '/media/'),
 ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 SITE_ID = 1
