fb3c43bf460b759a6c0c8ee01f0a22e4184acdb3
Rolandas <rolandaswb@gmail.com>
2020-04-06 16:05:47 +0300

    register htmlfeature

--- api_blog/admin.py
+++ api_blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from api_blog.models import Post, Feature, Insight
+from api_blog.models import Post, Feature, Insight, HtmlStylingChange
 
 
 admin.site.register(Post)
 admin.site.register(Insight)
 admin.site.register(Feature)
+admin.site.register(HtmlStylingChange)
