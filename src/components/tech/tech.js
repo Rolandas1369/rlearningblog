10ef708ad9d9484a3db76a3dca5fad7a9cc38a7a
Rolandas <rolandaswb@gmail.com>
2020-04-21 15:36:48 +0300

    tech styling

--- src/components/tech/tech.js
+++ src/components/tech/tech.js
@@ -42,17 +42,17 @@ export default class App extends Component {
         return workArraySorted.map((wrk) => {
             let helpers = (wrk.skills_used).split(',')        
             return (
-                <div key={Math.random()} className="flex-full work-exp border-t-2">                   
+                <div key={Math.random()} className="flex work-exp border-t-2">                   
                        
 
-                            <div className="w-1/2">
+                            <div className="exp-text">
                                 <div className="w-full h-12 pt-1">{wrk.worked_from}</div>
                                 <div className="w-full pt-1">{wrk.description}</div>
                                 <div className="w-full mt-1 pt-1 mt-3 mb-3">
  
                                     {helpers.length > 1 ? <p className="pb-3">In this project I used:</p> : null}
-                                    <div className="flex-col">
-                                        {helpers.length > 1 ? helpers.map((helper) => <span key={Math.random()} className="skill-span bg-teal-500 mr-3 p-1"> {helper}</span>) : null}
+                                    <div className="description-image">
+                                        {helpers.length > 1 ? helpers.map((helper) => <div key={Math.random()} className="skill-span bg-teal-500 p-2 mr-3 mt-4 mb-4"> {helper}</div>) : null}
                                     </div>
                                 </div>
                                 <div className="w-full pt-3 pb-3">
@@ -73,14 +73,14 @@ export default class App extends Component {
 
         let workexperiece = this.createExp()
         return (     
-            <div className="w-full p-10">
+            <div className="w-full pr-10 pl-10 pb-10">
 
                 <div className="flex">
-                    <div className="name-header w-1/2">
+                    <div className="name-header pt-10">
                         <h1 className="text-5xl">Rolandas Butkevičius</h1>
                         <h2 className="text-center border-b-2 mr-10 ml-10">Web developer harnessing python and javascript technologies</h2>
                     </div>
-                    <div className="contacts-header w-1/2">
+                    <div className="contacts-header pt-5">
                         <div><i className="fa fa-envelope"></i><a href="mailto:rolandaswb@gmail.com">rolandaswb@gmail.com</a></div>
                         <div><i className="fab fa-skype"></i>rolandaswb@gmail.com</div>
                         <div><i className="fab fa-linkedin"></i>
@@ -98,8 +98,8 @@ export default class App extends Component {
                     </div>
                 </div>
                     
-                <div className="top w-full mt-20">
-                    <div className="w-1/2 workflow">
+                <div className="top flex w-full ">
+                    <div className="workflow pt-5">
                         <h3 className="font-bold">When I code</h3>
                         <ul className="pt-6">
                             <li>I use <b>Linux</b> as working OS. </li>
@@ -110,7 +110,7 @@ export default class App extends Component {
                             <li>Testing for runing code with less bugs</li>
                         </ul>
                     </div>
-                    <div className="w-1/2 languages">
+                    <div className="languages pt-5">
                         <h3 className="font-bold">Languages I can communicate with</h3>
                         <ul className="pt-6">
                             <li>Russian</li>
@@ -121,7 +121,7 @@ export default class App extends Component {
                 </div>
 
                 <div className="work-experience">
-                    <h3 className="text-4xl">Programming / Work History</h3>
+                    <h3 className="text-4xl pt-5">Programming / Work History</h3>
                     { workexperiece }
                 </div>
 
@@ -136,7 +136,7 @@ const ImageDispl = (props) => {
         if(props.image !== null) {
             url = (props.image).slice(0, props.image.indexOf('?'))
             return (
-                <div className="portolio-image w-1/2 pt-2 pb-2">
+                <div className="portolio-image pt-2 pb-2">
                     <a href={url} target="_blank" rel="noopener noreferrer">                         
                         <img     
                             src={url} 
