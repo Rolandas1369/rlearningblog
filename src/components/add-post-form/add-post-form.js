d927dc820ac53eb6b2f4f3199716b307679a86ce
Rolandas1369 <rolandaswb@gmail.com>
2020-04-26 12:16:51 +0300

    add styles to create form

--- src/components/add-post-form/add-post-form.js
+++ src/components/add-post-form/add-post-form.js
@@ -1,95 +1,161 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './add-post-form.css';
+import "./add-post-form.css";
 
-export default class AddPostForm extends Component { 
+export default class AddPostForm extends Component {
+  state = {
+    title: "",
+    content: "",
+    file: null,
+    filename: "",
+    gist_id: "",
+    gist_filename: "",
+    video_src: "",
+    lang_choice: "Both", // Set default language choice to be Both
+  };
 
-    state = {
-        title: '',
-        content: '',
-        file: null,
-        filename: '',
-        gist_id: '',
-        gist_filename: '',
-        video_src:'',
-        lang_choice: 'Both' // Set default language choice to be Both
-    }
+  handleFile = (e) => {
+    let file = e.target.files[0];
+    this.setState({ file: file, filename: file.name });
+  };
 
-    handleFile = (e) => {
-        let file = e.target.files[0]
-        this.setState({file: file, filename: file.name})
-    }
+  handleFormSubmit = async (event) => {
+    event.preventDefault();
+  };
 
-    handleFormSubmit = async (event) => {
-        event.preventDefault()
-    }
+  onFieldChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    onFieldChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value})
-    }
+  addImage = (e) => {
+    this.setState({ item: e.target.value });
+  };
 
-    addImage = (e) => {
-        this.setState({ item: e.target.value})
-    }
+  componentDidMount = () => {
+    this.setState({ title: "title", content: "content", file: null });
+  };
 
-    componentDidMount = () =>  {
-        this.setState({title: "title", content: "content", file: null})
-    } 
+  addBold = () => {
+    this.setState({ content: this.state.content + " [b] [/b]" });
+  };
 
-    addBold = () => {
-        this.setState({content: this.state.content + " [b] [/b]"})
-    }
+  addPreTag = () => {
+    this.setState({ content: this.state.content + " [pre] [/pre]" });
+  };
 
-    addPreTag = () => {
-        this.setState({content: this.state.content + " [pre] [/pre]"})
-    }
+  onOptionChange = (e) => {
+    this.setState({ lang_choice: e.target.value });
+  };
 
-    onOptionChange = (e) => {
-        this.setState({lang_choice: e.target.value})
-    }
+  render() {
+    const {
+      title,
+      content,
+      file,
+      filename,
+      gist_id,
+      gist_filename,
+      video_src,
+      lang_choice,
+    } = this.state;
 
+    return (
+      <div className="add-post-div">
+        <form
+          className="add-post-form"
+          onSubmit={(e) => this.handleFormSubmit(e)}
+          encType="multipart/form-data"
+        >
+          <div className="tags-pallete flex-col">
+            <button
+              className="bold-item bg-blue-500 hover:bg-blue-700 text-white font-bold"
+              value={this.state.content}
+              onClick={(e) => this.addBold(e)}
+            >
+              Bold
+            </button>
+            <button
+              className="pre-item bg-blue-500 hover:bg-blue-700 text-white font-bold"
+              value={this.state.content}
+              onClick={() => this.addPreTag()}
+            >
+              Pre
+            </button>
+          </div>
+          <a href="/" className="">
+            Home
+          </a>
+          <h2>Label for content</h2>
 
-    render(){
-        const {title, content, file, filename,
-               gist_id, gist_filename, video_src, 
-               lang_choice} = this.state
+          <input
+            className="form-input bg-gray-200"
+            onChange={(e) => this.onFieldChange(e)}
+            type="text"
+            name="title"
+          />
+          <h3>Content</h3>
+          <textarea
+            className="bg-gray-200"
+            onChange={this.onFieldChange}
+            type="text"
+            name="content"
+            value={this.state.content}
+          ></textarea>
+          <h3>Gist id</h3>
+          <input
+            className="gist-input bg-gray-200"
+            onChange={(e) => this.onFieldChange(e)}
+            type="text"
+            name="gist_id"
+          />
+          <h3>Gist file name</h3>
+          <input
+            className="gist-filename-input bg-gray-200"
+            onChange={(e) => this.onFieldChange(e)}
+            type="text"
+            name="gist_filename"
+          />
+          <h3>Video ID</h3>
+          <input
+            className="video-src-input bg-gray-200"
+            onChange={(e) => this.onFieldChange(e)}
+            type="text"
+            name="video_src"
+          />
+          <h3>Select language</h3>
+          <select name="lang_choice" onChange={(e) => this.onFieldChange(e)}>
+            <option>---</option>
+            <option>Python</option>
+            <option>React</option>
+            <option>Both</option>
+          </select>
 
-        
-        return(
-            <div className="add-post-div">
-                <form className="add-post-form" onSubmit={(e) => this.handleFormSubmit(e)} encType="multipart/form-data">
-                    <div className="tags-pallete">
-                        <button className="bold-item" value={this.state.content} onClick={(e) => this.addBold(e)}>Bold</button>
-                        <button className="pre-item" value={this.state.content} onClick={() => this.addPreTag()}>Pre</button>
-                    </div>
-                    <a href="/">Home</a>
-                    <h2>Label for content</h2>
-                    
-                    <input className="form-input" onChange={(e) => this.onFieldChange(e)} type="text" name='title'/>
-                    <h3>Content</h3>
-                    <textarea onChange={this.onFieldChange} type="text" name='content' value={this.state.content}></textarea>
-                    <h3>Gist id</h3>
-                    <input className="gist-input" onChange={(e) => this.onFieldChange(e)} type="text" name='gist_id'/>
-                    <h3>Gist file name</h3>
-                    <input className="gist-filename-input" onChange={(e) => this.onFieldChange(e)} type="text" name='gist_filename'/>
-                    <h3>Video ID</h3>
-                    <input className="video-src-input" onChange={(e) => this.onFieldChange(e)} type="text" name='video_src'/>
-                    <h3>Select language</h3>
-                    <select name="lang_choice" onChange={(e) => this.onFieldChange(e)}>
-                        <option>---</option>
-                        <option>Python</option>
-                        <option>React</option>
-                        <option>Both</option>
-                    </select>
-
-                    <button id="submit-button" className="submit-button" onClick={() => this.props.addItem(title, content, 
-                                                                                 file, filename, 
-                                                                                 gist_id, gist_filename, 
-                                                                                 video_src, lang_choice)}
-                                                                                 >Add post</button>
-                    <input className="file-upload-button" onChange={(e) => this.handleFile(e)} type="file" name="file"/>
-                </form>
-            </div>
-        )
-    }
-} 
+          <button
+            id="submit-button"
+            className="submit-button bg-blue-500 hover:bg-blue-700 text-white font-bold"
+            onClick={() =>
+              this.props.addItem(
+                title,
+                content,
+                file,
+                filename,
+                gist_id,
+                gist_filename,
+                video_src,
+                lang_choice
+              )
+            }
+          >
+            Add post
+          </button>
+          <input
+            className="file-upload-button"
+            onChange={(e) => this.handleFile(e)}
+            type="file"
+            name="file"
+          />
+        </form>
+      </div>
+    );
+  }
+}
--- src/components/video-display/video-display.js
+++ src/components/video-display/video-display.js
@@ -1,58 +1,39 @@
-import React from 'react';
-import YouTube from 'react-youtube';
- 
+import React from "react";
+import YouTube from "react-youtube";
+
 export default class VideoDisplay extends React.Component {
-
-
-
-    showVideo = (videoUrl, opts) => {
-        if (videoUrl !== null && videoUrl !== undefined) {
-            return (
-                <YouTube
-                    videoId={videoUrl}
-                    opts={opts}
-                    onReady={this._onReady}
-                />
-            )
-        } else {
-            return null;
-        }
-    } 
-    //https://youtu.be/ZvXskTArKKs?list=RDZvXskTArKKs
+  showVideo = (videoUrl, opts) => {
+    if (videoUrl !== null && videoUrl !== undefined) {
+      return <YouTube videoId={videoUrl} opts={opts} onReady={this._onReady} />;
+    } else {
+      return null;
+    }
+  };
+  //https://youtu.be/ZvXskTArKKs?list=RDZvXskTArKKs
   render() {
     const opts = {
-      height: '390',
-      width: '640',
-      playerVars: { // https://developers.google.com/youtube/player_parameters
-        autoplay: 0
-      }
+      height: "390",
+      width: "640",
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 0,
+      },
     };
 
-    
-    let videoUrl = this.props.item.video_src
+    let videoUrl = this.props.item.video_src;
 
-
-    let video = null
-    if(videoUrl === ""){
-      video = null
-    }
-    else{
-      video = this.showVideo(videoUrl, opts)
+    let video = null;
+    if (videoUrl === "") {
+      video = null;
+    } else {
+      video = this.showVideo(videoUrl, opts);
     }
 
-
- 
-    return (
-
-    <div>{video}</div>
-
-      
-    );
+    return <div className="video-holder">{video}</div>;
   }
- 
+
   _onReady(event) {
     // access to player in all event handlers via event.target
     event.target.pauseVideo();
   }
 }
- 
